3fe927815c37754b45dd9183d100c760ad72b893
Carolina <carolina.reis@nava.com.br>
2022-04-13 17:32:22 -0300

    Teste da Service com o JUnit

---
+++ primeiro-ms/src/test/java/br/com/nava/controllers/UsuarioControllerTests.java
@@ -0,0 +1,17 @@
+package br.com.nava.controllers;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UsuarioControllerTests {
+	
+	private MockMvc mockMVC;
+	
+
+}
